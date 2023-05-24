fb4148692ea179bdc4dae72ad78438be764d8925
Zuzike Nemesová <darigjorkinen@gmail.com>
2023-05-24 18:43:14 +0200

    domaci ukoly 2-4

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,3 +1,5 @@
+//LEKCE 1
+/*
 describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
@@ -10,3 +12,61 @@ describe('Homework', async () => {
     });
 
 });
+//LEKCE 2
+describe('Czechitas Find Elements', async () => {
+
+    it('should open registrace page & find elements', async () => {
+
+        await browser.reloadSession();
+
+        await browser.url('/registrace');
+
+       const wholeNameField = $('input#name');
+       console.log(await wholeNameField.getHTML());
+
+       const emailField = $('#email');
+       console.log(await emailField.getHTML());
+
+       const passwField = $('[name="password"]]');
+       console.log(await passwField.getHTML());
+
+       const passwCheckField = $('#password-confirm');
+       console.log(await passwCheckField.getHTML());
+
+       const registrationButton = $('button.btn-primary');
+       console.log(await registrationButton.getText());
+            
+    });
+
+});
+
+//LEKCE 3
+describe('Homework', async () => {
+
+    it('should login on registration page', async () => {
+
+        await browser.reloadSession();
+
+        await browser.url('/registrace');
+        
+       const wholeNameField = $('input#name');
+       await wholeNameField.setValue('Boba Domáca');
+
+       const emailField = $('#email');
+       await emailField.setValue('boba@gmail.com');
+
+       const passwField = $('[name="password"]]');
+       await passwField.setValue('heslo123')
+
+       const passwCheckField = $('#password-confirm');
+       await passwCheckField.setValue('heslo123')
+
+       const registrationButton = $('button.btn-primary');
+       await registrationButton.click();
+
+    });
+
+});*/
+
+//LEKCE 4
+
