01ad0fddf565434bc6bbf9ad0bf95582bc73a1ff
Zuzike Nemesová <darigjorkinen@gmail.com>
2023-06-14 17:33:04 +0200

    DU update

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,5 +1,7 @@
-//LEKCE 1
+import {username, password} from '../fixtures.js'
 
+//LEKCE 1
+/*
 describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
@@ -55,7 +57,7 @@ describe('Homework', async () => {
        const emailField = $('#email');
        await emailField.setValue('boba@gmail.com');
 
-       const passwField = $('[name="password"]]');
+       const passwField = $('[name="password"]');
        await passwField.setValue('heslo123')
 
        const passwCheckField = $('#password-confirm');
@@ -65,8 +67,114 @@ describe('Homework', async () => {
        await registrationButton.click();
 
     });
-
 });
 
 //LEKCE 4
 
+describe('Homework - registration success', async () => {
+
+    beforeEach(async ()=>{
+        await browser.reloadSession();
+        await browser.url('/registrace');
+    });
+
+    it('open registration page and check', async () => {
+        
+        const wholeNameField = $('input#name');
+        const emailField = $('#email');
+        const passwField = $('[name="password"]');
+        const passwCheckField = $('#password-confirm');
+        const registrationButton = $('button.btn-primary');
+
+        console.log('Name field visibility status: ' + await wholeNameField.isDisplayed());
+        console.log('Name field activation status:' + await wholeNameField.isEnabled());
+        
+        console.log('Email field visibility status: ' + await emailField.isDisplayed());
+        console.log('Email field activation status: ' + await emailField.isEnabled());
+
+        console.log('Password field visibility status: ' + await passwField.isDisplayed());
+        console.log('Password field activation status: ' + await passwField.isEnabled());
+
+        console.log('PasswordControl field visibility status: ' + await passwCheckField.isDisplayed());
+        console.log('PasswordControl field activation status: ' + await passwCheckField.isEnabled());
+
+        console.log('Button visibility: ' + await registrationButton.isDisplayed());
+        console.log('Button activation: ' + await registrationButton.isEnabled());
+    });
+    
+    it('user registration and validity check', async () => {
+
+        const wholeNameField = $('input#name');
+        await wholeNameField.setValue('Bobana Domáca');
+ 
+        const emailField = $('#email');
+        await emailField.setValue('bobana@gmail.com');
+ 
+        const passwField = $('#password');
+        await passwField.setValue('Heslo123')
+ 
+        const passwCheckField = $('#password-confirm');
+        await passwCheckField.setValue('Heslo123')
+ 
+        const registrationButton = $('button.btn-primary');
+        await registrationButton.click();
+
+        const userNameDropdown = $('.navbar-right').$('[data-toggle="dropdown"]');
+        console.log('User currently logged in: ' + await userNameDropdown.getText());
+
+    });
+});
+
+describe('Homework - registration failur', async () => {
+
+    beforeEach(async ()=>{
+        await browser.reloadSession();
+        await browser.url('/registrace');
+    });
+
+    it('user registration with existing email and feedback', async () => {
+
+        const wholeNameField = $('input#name');
+        await wholeNameField.setValue('Czechitová Janka');
+ 
+        const emailField = $('#email');
+        await emailField.setValue('da-app.admin@czechitas.cz');
+ 
+        const passwField = $('input#password');
+        await passwField.setValue('CzHeslo1234')
+ 
+        const passwCheckField = $('#password-confirm');
+        await passwCheckField.setValue('CzHeslo1234')
+ 
+        const registrationButton = $('button.btn-primary');
+        await registrationButton.click();
+
+        const feedbackAlert = $('.invalid-feedback');
+        console.log('Alert: ' + await feedbackAlert.getText());
+
+    });
+
+    it('user registration with invalid password and feedback', async () => {
+
+        const wholeNameField = $('input#name');
+        await wholeNameField.setValue('Ja Somsom');
+ 
+        const emailField = $('#email');
+        await emailField.setValue('mammail@gmail.com');
+ 
+        const passwField = $('[name="password"]');
+        await passwField.setValue('123451234')
+ 
+        const passwCheckField = $('#password-confirm');
+        await passwCheckField.setValue('123451234')
+ 
+        const registrationButton = $('button.btn-primary');
+        await registrationButton.click();
+        
+        const feedbackAlert = $('.invalid-feedback');
+        console.log('Alert: ' + await feedbackAlert.getText());
+
+    });
+});*/
+
+//LEKCE 5
