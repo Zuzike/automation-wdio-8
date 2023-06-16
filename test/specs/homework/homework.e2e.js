42392983a7488b717bba2e36bc0150cb7e2ccced
Zuzike Nemesová <darigjorkinen@gmail.com>
2023-06-16 10:43:11 +0200

    DU 5 added

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -122,6 +122,8 @@ describe('Homework - registration success', async () => {
         const userNameDropdown = $('.navbar-right').$('[data-toggle="dropdown"]');
         console.log('User currently logged in: ' + await userNameDropdown.getText());
 
+        //const userNameDropdown2 = $('.dropdown').$('span');
+
     });
 });
 
@@ -175,6 +177,112 @@ describe('Homework - registration failur', async () => {
         console.log('Alert: ' + await feedbackAlert.getText());
 
     });
-});*/
+});
 
 //LEKCE 5
+describe('Homework - registration success', async () => {
+
+    beforeEach(async ()=>{
+        await browser.reloadSession();
+        await browser.url('/registrace');
+    });
+
+    it('open registration page and check', async () => {
+        
+        const wholeNameField = $('input#name');
+        const emailField = $('#email');
+        const passwField = $('[name="password"]');
+        const passwCheckField = $('#password-confirm');
+        const registrationButton = $('button.btn-primary');
+
+        await expect(wholeNameField).toBeDisplayed();
+        await expect(wholeNameField).toBeEnabled();
+
+        await expect(emailField).toBeDisplayed();
+        await expect(emailField).toBeEnabled();
+
+        await expect(passwField).toBeDisplayed();
+        await expect(passwField).toBeEnabled();
+
+        await expect(passwCheckField).toBeDisplayed();
+        await expect(passwCheckField).toBeEnabled();
+
+        await expect(registrationButton).toBeDisplayed();
+        await expect(registrationButton).toBeClickable();
+    });
+    
+    xit('user registration and validity check', async () => {
+
+        const wholeNameField = $('input#name');
+        await wholeNameField.setValue('Solo Lomos');
+ 
+        const emailField = $('#email');
+        await emailField.setValue('solo@gmail.com');
+ 
+        const passwField = $('#password');
+        await passwField.setValue('Heslo123')
+ 
+        const passwCheckField = $('#password-confirm');
+        await passwCheckField.setValue('Heslo123')
+ 
+        const registrationButton = $('button.btn-primary');
+        await registrationButton.click();
+
+        const loggedUser = $('.navbar-right').$('[data-toggle="dropdown"]');
+        await expect(await loggedUser.getText()).toEqual('Solo Lomos');
+    });
+});
+
+describe('Homework - registration failur', async () => {
+
+    beforeEach(async ()=>{
+        await browser.reloadSession();
+        await browser.url('/registrace');
+    });
+
+    it('user registration with existing email and feedback', async () => {
+
+        const wholeNameField = $('input#name');
+        await wholeNameField.setValue('Czechitová Janka');
+ 
+        const emailField = $('#email');
+        await emailField.setValue('da-app.admin@czechitas.cz');
+ 
+        const passwField = $('input#password');
+        await passwField.setValue('CzHeslo1234')
+ 
+        const passwCheckField = $('#password-confirm');
+        await passwCheckField.setValue('CzHeslo1234')
+ 
+        const registrationButton = $('button.btn-primary');
+        await registrationButton.click();
+
+        const feedbackAlert = $('.invalid-feedback');
+        await expect (feedbackAlert).toHaveText('Účet s tímto emailem již existuje');
+
+    });
+
+    it('user registration with invalid password and feedback', async () => {
+
+        const wholeNameField = $('input#name');
+        await wholeNameField.setValue('Ja Somsom');
+ 
+        const emailField = $('#email');
+        await emailField.setValue('mammail@gmail.com');
+ 
+        const passwField = $('[name="password"]');
+        await passwField.setValue('123451234')
+ 
+        const passwCheckField = $('#password-confirm');
+        await passwCheckField.setValue('123451234')
+ 
+        const registrationButton = $('button.btn-primary');
+        await registrationButton.click();
+        
+        const feedbackAlert = $('.invalid-feedback');
+        await expect (feedbackAlert).toHaveText('Heslo musí obsahovat minimálně 6 znaků, velké i malé písmeno a číslici');
+    });
+});*/
+
+//LEKCE 6
+
